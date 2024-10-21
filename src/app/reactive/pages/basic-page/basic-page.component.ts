be3e13913ea6e8375a8710c8632a137864e8f626
Leo Ss <leo.vm.cba@gmail.com>
2024-10-21 10:08:19 -0300

    add: show validation errors

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -31,8 +31,34 @@ export class BasicPageComponent implements OnInit{
     this.myForm.reset( defaultForm )
   }
 
+  isValidField( field: string ):boolean | null{
+
+    return this.myForm.controls[field].errors && this.myForm.controls[field].touched;
+  }
+
+  getFieldError( field: string ):string | null {
+    if( !this.myForm.controls[field] ) return null;
+
+    const errors = this.myForm.controls[field].errors || { }
+
+    for (const key of Object.keys(errors)) {
+      switch (key){
+        case 'required':
+          return "Este campo es requerido."
+        case 'minlength':
+          return `El campo debe tener al menos ${ errors['minlength'].requiredLength } caracteres.`
+        default:
+          return null
+      }
+    }
+    return null
+  }
+
   onSave(): void {
-    if( this.myForm.invalid ) return
+    if( this.myForm.invalid ){
+      this.myForm.markAllAsTouched();
+      return;
+    }
 
     console.log(this.myForm.value);
 
